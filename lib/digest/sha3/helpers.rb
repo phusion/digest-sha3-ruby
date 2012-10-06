2901fd374ab4f0e059824cac9c4c66384ef901a6
Hongli Lai (Phusion) <hongli@phusion.nl>
2012-10-06 12:02:57 +0200

    Force ascii encoding on hexdigest output

--- lib/digest/sha3/helpers.rb
+++ lib/digest/sha3/helpers.rb
@@ -1,9 +1,20 @@
+# encoding: ascii
 Digest::SHA3.class_eval do
   def self.hexdigest(*args)
-    digest(*args).unpack("H*").first
+    force_ascii(digest(*args).unpack("H*").first)
   end
 
   def hexdigest
-    digest.unpack("H*").first
+    Digest::SHA3.force_ascii(digest.unpack("H*").first)
+  end
+
+  if ''.respond_to?(:force_encoding)
+    def self.force_ascii(str)
+      str.force_encoding('ascii')
+    end
+  else
+    def self.force_ascii(str)
+      str
+    end
   end
 end
